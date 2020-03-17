afafa25049f100be2a3c6c1c3ed7eeddd3c8d1cb
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2020-03-17 14:34:39 -0300

    terminando a fase de requisitos

--- estagio/Requisitos.py
+++ estagio/Requisitos.py
@@ -1,10 +1,52 @@
+import time
+
 class Requisitos:
-    def _init_(self, req_funcionais, req_nfuncionais, req_dominio, req_dados, regra_negocio):
-        self.req_funcionais = req_funcionais
-        self.req_nfuncionais = req_nfuncionais
-        self.req_dominio = req_dominio
-        self.req_dados = req_dados
-        self.regra_negocio =regra_negocio
+    def _init_(self, requisitos):
+        self.requisitos = [1, 2, 3, 4, 5, 6]
+
+    def Faserequisitos(self, tipo_requisitos):       
+        for requisito in self.requisitos:
+            print('requisito:' + requisito)
+
+        return 
+
+    def printaFase(self, faseRequistos):
+    ##Processe de engenharia de requisitos
+        print("Iniciando os requisitos, aguarde")
+        time.sleep(1.5) 
+
+    ##Estudo de viabilidade da requisição
+        print("Incio do estudo de viabilidade, aguarde")
+        time.sleep(1.5)
+
+        print("Confirmação da viabilidade do softer requerido, aguarde")
+        time.sleep(1.5)
+    #Elicitação e análise de requisitos
+        print("Discussão com usuário, aguarde")
+        time.sleep(1.5)
+
+        print("Compreendendo o sistema solicitado, aguarde")
+        time.sleep(1.5)
+
+        print("Desenvolvimento de prototipo, aguarde")
+        time.sleep(2)
+
+        print("Recebendo os requisitos" + self.requisitos)
+        time.sleep(2)
+
+        #Os requisitos
+        print('Entendendo os requisitos, aguarde')
+        time.sleep(2)
+
+        print('Requisitos funcionais: '+ {self.requisitos[1]})
+
+    
+
+
+       
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 from estagio import Requisitos
+
+print(printaFase)
