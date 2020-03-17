79ffaa1e3b716dbfdc3456d0e2963125c18dba7f
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2020-03-17 09:16:48 -0300

    first commit

---
+++ estagio/Requisitos.py
@@ -0,0 +1,20 @@
+class Requisitos:
+    def _init_(self, req_funcionais, req_nfuncionais, req_dominio, req_dados, regra_negocio):
+        self.req_funcionais = req_funcionais
+        self.req_nfuncionais = req_nfuncionais
+        self.req_dominio = req_dominio
+        self.req_dados = req_dados
+        self.regra_negocio =regra_negocio
+
+
+
+        ##criar uma array com todas as posições dos Requisitos
+        ##printar cada fase dos requisitos
+
+       
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,1 @@
+from estagio import Requisitos
