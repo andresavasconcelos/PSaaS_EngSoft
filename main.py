016d5ea2733ff794eaa405e5aa2378908c461017
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2020-03-17 17:28:27 -0300

    corrigindo import

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from estagio import Requisitos
-from estagio import Projetos
-from estagio import Codificacao
-from estagio import Validacao
-from estagio import Implementacaco 
+from estagio.Requisitos import Requisitos
 
-class Main:
+
+
 #fase requisitos
+
+Requisitos.Faserequisitos(faseRequistos)
 
 #fase de projeto
 
