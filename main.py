e917453bf90d344e6f640d95f186cbc0b0105f65
guilhermebailoni <62307706+guilhermebailoni@users.noreply.github.com>
2020-03-18 17:27:30 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from estagio.Requisitos import Requisitos
 from estagio.Projetos import Projetos
 from estagio.Codificacao import Codificacao
+from estagio.Testes import Testes
+import time
 
 class Main:
     print("Ola")
     req = Requisitos()
     pro = Projetos()
     cod = Codificacao()
-    cod.codificacaoFuncionalidades(pro.criacaoProjeto(req.printaFase()))
+
+    def chamaTeste(projeto, funcionalidades):
+        tes = Testes()
+        if(tes.testeDoSistema(funcionalidades) == True):
+            print("Colocando em producao")
+            #Chama implementacao
+            time.sleep(1)
+        else:
+            print("Retornando para codificacao")
+            funcionalidades = cod.codificacaoFuncionalidades(projeto)
+            self.chamaTeste(projeto, funcionalidades)
+            time.sleep(1)
+
+    requisitos = req.printaFase()
+    projeto = pro.criacaoProjeto(requisitos)
+    funcionalidades = cod.codificacaoFuncionalidades(projeto)
+    chamaTeste(projeto,funcionalidades)
 
 
 #fase requisitos
