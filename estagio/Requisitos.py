805981108e31da10633aeb7c048f769566e4e2cd
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2020-03-17 22:02:30 -0300

    finalizando requisitos

--- estagio/Requisitos.py
+++ estagio/Requisitos.py
@@ -12,17 +12,17 @@ class Requisitos:
     def printaFase(self):
         requisitos = [1, 2, 3, 4, 5, 6]
               
-    ##Processe de engenharia de requisitos
+        ##Processe de engenharia de requisitos
         print("Iniciando os requisitos, aguarde")
         time.sleep(1.5) 
 
-    ##Estudo de viabilidade da requisição
+        ##Estudo de viabilidade da requisição
         print("Incio do estudo de viabilidade, aguarde")
         time.sleep(1.5)
 
         print("Confirmação da viabilidade do softer requerido, aguarde")
         time.sleep(1.5)
-    #Elicitação e análise de requisitos
+        #Elicitação e análise de requisitos
         print("Discussão com usuário, aguarde")
         time.sleep(1.5)
 
@@ -44,6 +44,31 @@ class Requisitos:
         print('Requisitos de domínio: '+ str(requisitos[2]))
         print('Requisitos de dados: '+ str(requisitos[3]))
         print('Requisitos regra de negócio: '+ str(requisitos[4]))
+
+        #Fase de prioridades        
+        print('Prioridades definidas')
+        time.sleep(2)
+
+        #Fase de Especificação
+        print('Definindo a especificação do Software ...')
+        print('Definindo as restrições do Softwsre ...')
+        time.sleep(3) 
+        print('Especificação definida!') 
+        
+        #Fase de modelagem
+        print('Definindo modelagem do Software ...')
+        print('Definindo modelo UML ...')
+        print('Encadeando cenário ...')
+        time.sleep(3)
+        print('Modelagem definida!')
+
+        #fase de validacao de requisitos
+        print('Verificando a validade do software ...')
+        print('verificando a consistencia ...')
+        print('verificando o realismo ...')
+        print('Validações em processamento, aguarde ...')
+        time.sleep(3)
+        print('Validações efetuada com sucesso')
 
         pass
 
