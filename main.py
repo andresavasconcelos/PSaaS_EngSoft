5c61f0fdd4ee8c273d0f8d6030dd494928060d30
guilhermebailoni <62307706+guilhermebailoni@users.noreply.github.com>
2020-03-17 23:34:25 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from estagio.Requisitos import Requisitos
 from estagio.Projetos import Projetos
+from estagio.Codificacao import Codificacao
 
 class Main:
     print("Ola")
     req = Requisitos()
     pro = Projetos()
-    pro.criacaoProjeto(req.printaFase())
+    cod = Codificacao()
+    cod.codificacaoFuncionalidades(pro.criacaoProjeto(req.printaFase()))
 
 
 #fase requisitos
