d1ec409bdf3badf095cfd48fd2d3aae0991236df
guilhermebailoni <62307706+guilhermebailoni@users.noreply.github.com>
2020-03-19 03:41:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from estagio.Implementacao import Implementacao
 import time
 
 class Main:
-    print("Iniciando processo de Engenharia de Software ...")
-    time.sleep(3)
+    print("Iniciando processo de Engenharia de Software ...\n")
+    time.sleep(2)
     req = Requisitos()
     pro = Projetos()
     cod = Codificacao()
@@ -15,29 +15,54 @@ class Main:
 
     def chamaTeste(projeto, funcionalidades):
         tes = Testes()
-        if(tes.testeDoSistema(funcionalidades) == True):
-            print("Colocando em producao")
+        if(tes.teste(funcionalidades) == True):
+            print("\n")
             #Chama implementacao
             time.sleep(1)
         else:
-            print("Retornando para codificacao")
+            print("Retornando para codificacao\n")
             funcionalidades = cod.codificacaoFuncionalidades(projeto)
             self.chamaTeste(projeto, funcionalidades)
             time.sleep(1)
 
+    #fase requisitos
+
     requisitos = req.printaFase()
+
+    #fase de projeto
+    escolha = input("Etapa de requisitos concluida, deseja fazer o projeto? 'Sim' ou 'Nao'\n")
+    if(escolha == "Sim" or escolha == "sim"):
+        print("Iniciando etapa de projeto!")
+        time.sleep(1)
+    else:
+        print("Abandonando processo de criacao de software!")
+        raise SystemExit
     projeto = pro.criacaoProjeto(requisitos)
+
+    #fase de codificacao
+    escolha = input("Etapa de projeto concluida, deseja fazer a codificacao? 'Sim' ou 'Nao'\n")
+    if(escolha == "Sim" or escolha == "sim"):
+        print("Iniciando codificacao!")
+        time.sleep(1)
+    else:
+        print("Abandonando processo de criacao de software!")
+        raise SystemExit
     funcionalidades = cod.codificacaoFuncionalidades(projeto)
+
+    #fase de testes
+    escolha = input("Codificacao concluida, prosseguir para teste? 'Sim' ou 'Nao'\n")
+    if(escolha == "Sim" or escolha == "sim"):
+        print("Iniciando etapa de teste!")
+        time.sleep(1)
+    else:
+        escolha = input("Deseja realizar a implementacao sem teste? 'Sim' ou 'Nao'\n")
+        if(escolha == "Sim" or escolha == "sim"):
+            #fase de implementacao
+            print("Implementacao sem teste!")
+            time.sleep(1)
+            implementacao = imp.faseImplementacao(1)
+        raise SystemExit
+
     chamaTeste(projeto,funcionalidades)
+    #fase de implementacao
     implementacao = imp.faseImplementacao(1)
-
-
-#fase requisitos
-
-#fase de projeto
-
-#fase de construção
-
-#fase de testes
-
-#fase de implementacao
